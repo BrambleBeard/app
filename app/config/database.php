0582ac23e763ca8b3d04491a9f0fce7be1d61c22
Derek Nutile <dereknutile@gmail.com>
2015-12-08 14:10:19 -0800

    Adds connection and migrations table to env and populates in database.php.

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'bramblebeard'),
-            'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', 'root'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
@@ -98,7 +98,7 @@ return [
     |
     */
 
-    'migrations' => 'migrations',
+    'migrations' => env('DB_MIGRATIONS', 'migrations'),
 
     /*
     |--------------------------------------------------------------------------
