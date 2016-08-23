7632dc2eafbd5a196df75fb4acc64f926d8352b8
Derek Nutile <dereknutile@gmail.com>
2016-08-23 12:49:43 -0700

    add/view Create home index Delete standard welcome page.

---
+++ project/resources/views/home/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo env('APP_TITLE'); ?></title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title"><?php echo env('APP_TITLE'); ?></div>
+            </div>
+        </div>
+    </body>
+</html>
