93cea8d9c2c056043cf772b8dd12e8925a5b24ec
Derek Nutile <dereknutile@gmail.com>
2016-08-23 12:51:37 -0700

    edit/html Update php shortcode

--- project/resources/views/home/index.blade.php
+++ project/resources/views/home/index.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title><?php echo env('APP_TITLE'); ?></title>
+        <title><?= env('APP_TITLE'); ?></title>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title"><?php echo env('APP_TITLE'); ?></div>
+                <div class="title"><?= env('APP_TITLE'); ?></div>
             </div>
         </div>
     </body>
