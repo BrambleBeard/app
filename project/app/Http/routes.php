6da64dc411b0e672868ab987a006a9b851f9186a
Derek Nutile <dereknutile@gmail.com>
2016-08-25 16:46:04 -1000

    edit/routes Adds standard routing and placeholders

--- project/app/Http/routes.php
+++ project/app/Http/routes.php
@@ -12,5 +12,63 @@
 */
 
 Route::get('/', function () {
-    return view('home/index');
+    return view('pages/splash');
 });
+
+// Route::controllers([
+//   'auth' => 'Auth\AuthController',
+//   'password' => 'Auth\PasswordController'
+// ]);
+
+/*
+|--------------------------------------------------------------------------
+| Place routes that require authentication here
+|--------------------------------------------------------------------------
+*/
+Route::group(['middleware' => ['auth', 'onlyenabledusers']], function () {
+    // Route::get('/',['as'=>'home', 'uses'=>'PagesController@table']);
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| Place routes that require admin status here
+|--------------------------------------------------------------------------
+*/
+Route::group(['middleware' => ['auth', 'onlyenabledusers', 'onlyallowadmins']], function () {
+    // Route::get('account',['as'=>'account', 'uses'=>'Account\AccountController@index']);
+    //
+    // Route::get('admin',['as'=>'admin', 'uses'=>'Admin\DashboardController@index']);
+    //
+    // Route::get('admin/directory/excel',['as'=>'admin.directory.excel', 'uses'=>'Admin\DirectoryController@excel']);
+    // Route::resource('admin/directory','Admin\DirectoryController');
+    // Route::resource('admin/department','Admin\DepartmentController');
+    // Route::resource('admin/location','Admin\LocationController');
+    // Route::resource('admin/tag','Admin\TagController');
+    // Route::resource('admin/cms','Admin\CmsController');
+    // Route::resource('admin/message','Admin\MessageController');
+    // Route::resource('admin/users','Admin\UsersController');
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| Place routes that DO NOT require authentication here
+|--------------------------------------------------------------------------
+*/
+// Route::get('/splash',['as'=>'splash', 'uses'=>'PagesController@splash']);
+Route::get('sign-in',['as'=>'login', 'uses'=>'Auth\AuthController@getLogin']);
+Route::get('sign-out',['as'=>'logout', 'uses'=>'Auth\AuthController@getLogout']);
+Route::get('reset',['as'=>'reset', 'uses'=>'Auth\PasswordController@getEmail']);
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Unused routes
+|--------------------------------------------------------------------------
+*/
+// Authentication registration routes...
+// Route::get('auth/register', 'Auth\AuthController@getRegister');
+// Authentication password reset routes...
+// Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
