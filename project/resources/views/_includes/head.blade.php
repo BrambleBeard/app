943d86143284d62ac2f88472b47da5962b82b35c
Derek Nutile <dereknutile@gmail.com>
2016-08-27 07:05:48 -1000

    edit/html Add links to assets in head file

--- project/resources/views/_includes/head.blade.php
+++ project/resources/views/_includes/head.blade.php
@@ -3,9 +3,21 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>{{ Config::get('app.site_title') }}</title>
 
+<meta name="viewport" content="width=device-width">
 <meta name="description" content="{{ Config::get('app.site_description') }}">
+<link rel="canonical" href="http://bramblebeard.github.io//">
 
-<!-- <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet"> -->
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Web application to store secret information securely." />
+<meta property="og:url" content="http://bramblebeard.github.io/" />
+<meta property="og:site_name" content="BrambleBeard" />
+<meta property="og:image" content="{{ asset('/assets/ico/favicon.png') }}" />
+
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+<link rel="stylesheet" href="{{ asset('/assets/vendor/fontawesome/css/font-awesome.min.css') }}">
+<link rel="stylesheet" href="{{ asset('/assets/css/quill.snow.css') }}">
+<link rel="stylesheet" href="{{ asset('/assets/css/custom.min.css') }}">
 
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
