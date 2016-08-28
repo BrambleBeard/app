081e4ae21632b0a8f2351a99a0fc1795395e3241
Derek Nutile <dereknutile@gmail.com>
2016-08-28 07:16:58 -1000

    edit/php Create page controller functions

--- project/app/Http/Controllers/PagesController.php
+++ project/app/Http/Controllers/PagesController.php
@@ -10,9 +10,43 @@ class PagesController extends Controller
 {
     public function splash()
     {
-        // $title = "Home Page";
-        // $page_active = "home";
-        // return view('pages.home', compact('title','page_active'));
-        return view('pages.splash');
+        $title = "Home";
+        $page_active = "home";
+        return view('pages.splash', compact('title','page_active'));
+        // return view('pages.splash');
+    }
+
+    public function about()
+    {
+        $title = "About";
+        $page_active = "about";
+        // $content = Cms::where('tag','about')->where('status',1)->first();
+        return view('pages.about', compact('title','page_active', 'content'));
+    }
+
+    public function contact()
+    {
+        $title = "Contact";
+        $page_active = "contact";
+        return view('pages.contact', compact('title','page_active'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function storeContact(Request $request)
+    {
+        $message = new Message;
+        $message->name = $request->name;
+        $message->email = $request->email;
+        $message->subject = $request->subject;
+        $message->message = $request->message;
+
+        $message->save();
+        session()->flash('flash_success','Thank you for your message.');
+        return redirect()->route('home');
     }
 }
