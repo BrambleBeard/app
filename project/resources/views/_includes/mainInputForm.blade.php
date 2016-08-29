c75ad3b0ced4d594cd37a6155212f7422dcab5aa
Derek Nutile <dereknutile@gmail.com>
2016-08-29 06:45:03 -1000

    edit/html Update layout and splash page

---
+++ project/resources/views/_includes/mainInputForm.blade.php
@@ -0,0 +1,12 @@
+
+<form class="pure-form">
+    <div id="toolbar"></div>
+    <div id="rich-text-editor"></div>
+    <input class="pure-input-1 input-flatten" type="text" placeholder="Use an optional password">
+    <div class="action-bar">
+        <button class="pure-button">Save</button>
+        <button class="pure-button">Email</button>
+        <button class="pure-button">Create</button>
+    </div>
+</form>
+<!-- /.pure-form -->
--- project/resources/views/_layouts/splash.blade.php
+++ project/resources/views/_layouts/splash.blade.php
@@ -6,7 +6,7 @@
 
 <body>
 
-    <div class="page-content">
+    <div class="main">
         @yield('content')
     </div>
 
