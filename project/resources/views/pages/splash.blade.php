82dc73dfd880ea7aa6352f7d43fb04379466f149
Derek Nutile <dereknutile@gmail.com>
2016-08-31 13:50:29 -1000

    edit/html Add app tagline to the splash page

--- project/resources/views/pages/splash.blade.php
+++ project/resources/views/pages/splash.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="header">
         <h1>{{ Config::get('app.site_title') }}</h1>
+        <h2>{{ Config::get('app.tagline') }}</h2>
     </div>
     <div class="content">
         @include('_includes/mainInputForm', ['rteId' => $rteId])
