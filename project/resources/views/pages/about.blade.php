627704971d9332c2b2f90caf4114be7e39d62e08
Derek Nutile <dereknutile@gmail.com>
2016-08-31 14:07:06 -1000

    edit/html Make each page view consistent

--- project/resources/views/pages/about.blade.php
+++ project/resources/views/pages/about.blade.php
@@ -1,5 +1,11 @@
 @extends('_layouts/splash')
 
 @section('content')
-    <h2 class="page-title">{{ $title }}</h2>
+    <div class="header">
+        <h1 class="page-title">{{ $title }}</h1>
+    </div>
+    <div class="content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <!-- ./content -->
 @stop
--- project/resources/views/pages/contact.blade.php
+++ project/resources/views/pages/contact.blade.php
@@ -1,35 +1,38 @@
 @extends('_layouts/splash')
 
 @section('content')
-    <h2 class="page-title">{{ $title }}</h2>
+    <div class="header">
+        <h1 class="page-title">{{ $title }}</h1>
+    </div>
+    <div class="content">
+        {!! Form::open( ['route' => 'storeContact'] ) !!}
 
-    {!! Form::open( ['route' => 'storeContact'] ) !!}
+            <div class="form-group">
+                {!! Form::label('name', 'Name', ['class'=>'required']) !!}
+                {!! Form::text('name', null, ['required', 'class'=>'form-control', 'placeholder'=>'First Last']) !!}
+            </div>
 
-        <div class="form-group">
-            {!! Form::label('name', 'Name', ['class'=>'required']) !!}
-            {!! Form::text('name', null, ['required', 'class'=>'form-control', 'placeholder'=>'First Last']) !!}
-        </div>
+            <div class="form-group">
+                {!! Form::label('email', 'Email', ['class'=>'required']) !!}
+                {!! Form::email('email', null, ['required', 'class'=>'form-control', 'placeholder'=>'email@domain.com']) !!}
+            </div>
 
-        <div class="form-group">
-            {!! Form::label('email', 'Email', ['class'=>'required']) !!}
-            {!! Form::email('email', null, ['required', 'class'=>'form-control', 'placeholder'=>'email@domain.com']) !!}
-        </div>
+            <div class="form-group">
+                {!! Form::label('subject', 'Subject', ['class'=>'required']) !!}
+                {!! Form::select('subject', ['General message'=>'General message','Name correction, addition, or removal'=>'Name correction, addition, or removal'], null, ['required', 'class'=>'form-control']) !!}
+            </div>
 
-        <div class="form-group">
-            {!! Form::label('subject', 'Subject', ['class'=>'required']) !!}
-            {!! Form::select('subject', ['General message'=>'General message','Name correction, addition, or removal'=>'Name correction, addition, or removal'], null, ['required', 'class'=>'form-control']) !!}
-        </div>
+            <div class="form-group">
+                {!! Form::label('message', 'Message', ['class'=>'required']) !!}
+                {!! Form::textarea('message', null, ['required', 'class'=>'form-control', 'placeholder'=>'Your message here.']) !!}
+            </div>
 
-        <div class="form-group">
-            {!! Form::label('message', 'Message', ['class'=>'required']) !!}
-            {!! Form::textarea('message', null, ['required', 'class'=>'form-control', 'placeholder'=>'Your message here.']) !!}
-        </div>
+            <div class="form-group">
+                {!! Form::submit('Submit', ['class'=>'btn btn-primary']) !!}
+                <a href="{{ route('home') }}" class="btn btn-default">Cancel</a>
+            </div>
 
-        <div class="form-group">
-            {!! Form::submit('Submit', ['class'=>'btn btn-primary']) !!}
-            <a href="{{ route('home') }}" class="btn btn-default">Cancel</a>
-        </div>
-
-    {!! Form::close() !!}
-
+        {!! Form::close() !!}
+    </div>
+    <!-- ./content -->
 @stop
