1ffe5bb806f0a9918cdcdbe1197bb388685bf057
Derek Nutile <dereknutile@gmail.com>
2016-09-02 07:10:26 -1000

    edit/html Add rows to textarea

--- project/resources/views/pages/contact.blade.php
+++ project/resources/views/pages/contact.blade.php
@@ -17,7 +17,7 @@
             {!! Form::select('subject', ['General message'=>'General message','Name correction, addition, or removal'=>'Name correction, addition, or removal'], null, ['required', 'class'=>'pure-input-1']) !!}
 
             {!! Form::label('message', 'Message', ['class'=>'required']) !!}
-            {!! Form::textarea('message', null, ['required', 'class'=>'pure-input-1', 'placeholder'=>'Your message here.']) !!}
+            {!! Form::textarea('message', null, ['required', 'class'=>'pure-input-1', 'rows'=>4, 'placeholder'=>'Your message here.']) !!}
 
             <div class="action-bar">
                 <a href="{{ route('home') }}" class="pure-button">Cancel</a>
