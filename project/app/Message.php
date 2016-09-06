0a9f5e253289915e761b12a55d44c0f7f35b337f
Derek Nutile <dereknutile@gmail.com>
2016-09-06 06:39:03 -0700

    add/feature Create and test messages module

---
+++ project/app/Message.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    // protected $table = 'messages';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['first', 'last', 'email', 'subject', 'message'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
