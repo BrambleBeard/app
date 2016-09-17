441cecaa8091f16c109a0c045caafb301561cf3f
Derek Nutile <dereknutile@gmail.com>
2016-09-17 07:09:57 -0700

    edit/html Convert main form into laravel collective

--- project/resources/views/_includes/mainInputForm.blade.php
+++ project/resources/views/_includes/mainInputForm.blade.php
@@ -1,13 +1,12 @@
 
-
-<form class="pure-form">
+{!! Form::open( ['route' => 'storeSecret', 'class' => 'pure-form'] ) !!}
     <div id="toolbar"></div>
-    <textarea id="{{ $rteId }}"></textarea>
-    <input class="pure-input-1 input-flatten" type="text" placeholder="Use an optional password">
+    {!! Form::textarea('content', null, ['required', 'id'=>$rteId, 'placeholder'=>'Last']) !!}
+    {!! Form::text('passcode', null, ['required', 'class'=>'pure-input-1 input-flatten', 'placeholder'=>'Use an optional password']) !!}
     <div class="action-bar">
         <button class="pure-button"><i class="fa fa-save"></i>Save</button>
         <button class="pure-button"><i class="fa fa-envelope-o"></i>Email expiring URL</button>
         <button class="pure-button pure-button-primary"><i class="fa fa-fire"></i>Create expiring URL</button>
     </div>
-</form>
+{!! Form::close() !!}
 <!-- /.pure-form -->
