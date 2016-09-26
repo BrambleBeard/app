19ffd78efa1c45e76a5073f58df0448563d8f211
Derek Nutile <dereknutile@gmail.com>
2016-09-25 18:55:28 -0700

    edit/php Add form token and an html class

--- project/resources/views/_includes/mainInputForm.blade.php
+++ project/resources/views/_includes/mainInputForm.blade.php
@@ -1,8 +1,9 @@
 
 {!! Form::open( ['route' => 'storeSecret', 'class' => 'pure-form', 'novalidate'] ) !!}
     <!-- <div id="toolbar"></div> -->
-    {!! Form::textarea('content', null, ['required', 'id'=>$rteId, 'placeholder'=>'Last']) !!}
+    {!! Form::textarea('content', null, ['required', 'id'=>$rteId, 'class'=>'pure-input-1', 'placeholder'=>'Last']) !!}
     {!! Form::text('passcode', null, ['class'=>'pure-input-1 input-flatten', 'placeholder'=>'Use an optional password']) !!}
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <div class="action-bar">
         <button class="pure-button"><i class="fa fa-save"></i>Save</button>
         <button class="pure-button"><i class="fa fa-envelope-o"></i>Email expiring URL</button>
