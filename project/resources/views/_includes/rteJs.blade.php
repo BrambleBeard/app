05651d474cf41dae6b2008f10d1c651e1e5ddaed
Derek Nutile <dereknutile@gmail.com>
2017-05-28 12:02:40 -0700

    e/js Tune Tinymce editor settings

--- project/resources/views/_includes/rteJs.blade.php
+++ project/resources/views/_includes/rteJs.blade.php
@@ -5,6 +5,8 @@
         selector: '#{{ $rteId }}',
         plugins: [],
         menu: [],
-        toolbar: 'styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent',
+        plugins: "insertdatetime",
+        toolbar: 'styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | insertdatetime',
+        branding: false
     });
 </script>
