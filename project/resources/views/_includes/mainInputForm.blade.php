a8b5a30cee8c8c088118c18e5004b6cb9d69c69e
Derek Nutile <dereknutile@gmail.com>
2017-05-29 10:53:03 -0700

    e/php Code main input form

--- project/resources/views/_includes/mainInputForm.blade.php
+++ project/resources/views/_includes/mainInputForm.blade.php
@@ -1,13 +1,27 @@
 
 {!! Form::open( ['route' => 'storeSecret', 'class' => 'pure-form', 'novalidate'] ) !!}
-    <!-- <div id="toolbar"></div> -->
-    {!! Form::textarea('content', null, ['required', 'id'=>$rteId, 'class'=>'pure-input-1', 'placeholder'=>'Last']) !!}
-    {!! Form::text('passcode', null, ['class'=>'pure-input-1 input-flatten', 'placeholder'=>'Use an optional password']) !!}
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
-    <div class="action-bar">
-        <button class="pure-button"><i class="fa fa-save"></i>Save</button>
-        <button class="pure-button"><i class="fa fa-envelope-o"></i>Email expiring URL</button>
-        <button class="pure-button pure-button-primary"><i class="fa fa-fire"></i>Create expiring URL</button>
+
+    <div class="field">
+        <div class="control">
+            {!! Form::textarea('content', null, ['required', 'id'=>$rteId, 'class'=>'textarea', 'placeholder'=>'Enter your secret here ...']) !!}
+        </div>
     </div>
+    <!-- /.field -->
+
+    <div class="field">
+        <div class="control">
+            {!! Form::text('passcode', null, ['class'=>'input', 'placeholder'=>'Use an optional password']) !!}
+        </div>
+    </div>
+    <!-- /.field -->
+
+    <div class="field">
+        <div class="control">
+            <button type="submit" name="submit" class="button is-primary" title="Create Disposable Secret"><i class="fa fa-check"></i>Create</button>
+            <a href="auth" class="button is-default" title="Save Secret"><i class="fa fa-lock"></i>Save</a>
+        </div>
+    </div>
+    <!-- /.field -->
 {!! Form::close() !!}
 <!-- /.pure-form -->
