a4b67fc359cd13bfdbb0bd3dc39770b5c99a3e06
Derek Nutile <dereknutile@gmail.com>
2017-05-29 08:16:23 -0700

    e/js Remove legacy scripts
    Remove jQuery and quill.js.

--- project/resources/views/_includes/scripts.blade.php
+++ project/resources/views/_includes/scripts.blade.php
@@ -1,6 +1,4 @@
 
 <!-- Scripts -->
-<script src="{{ asset('assets/vendor/jquery/dist/jquery.min.js' ) }}"></script>
-<!-- <script src="http://cdn.quilljs.com/1.0.0-rc.0/quill.js"></script> -->
 <script src='//cdn.tinymce.com/4/tinymce.min.js'></script>
 <script src="{{ asset('assets/js/custom.js' ) }}"></script>
