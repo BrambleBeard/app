953d024eee18296057cc3af9b3a146adc0fb5f7c
Derek Nutile <dereknutile@gmail.com>
2017-05-31 06:44:06 -0700

    n/php Add footer include blade

---
+++ project/resources/views/_includes/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="hero is-dark is-medium">
+    <div class="hero-body">
+        <div class="container">
+            <div class="content has-text-centered">
+                <p>Find out more <a href="/about">about {{ Config::get('app.app_title') }}</a>, check out the <a href="{{ Config::get('app.app_source_url') }}" target="_blank">source code</a>, or <a href="/contact">connect with us</a>.
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
