ba140d7ca917f0af072de227421a8307926a8fea
Derek Nutile <dereknutile@gmail.com>
2017-05-30 21:27:05 -0700

    e/php Edit about page

--- project/resources/views/pages/about.blade.php
+++ project/resources/views/pages/about.blade.php
@@ -1,12 +1,11 @@
 @extends('_layouts/default')
 
 @section('content')
-    <div class="header">
-        <h1>{{ Config::get('app.app_title') }}</h1>
-        <h2>{{ $title }}</h2>
+<section class="section">
+    <div class="container container-narrow">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque pariatur molestias accusantium eos voluptatem ducimus, qui adipisci placeat, aliquam velit expedita ex magnam dolorem maxime tempora. Vero voluptate maiores expedita!</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo facilis, labore nam animi natus, in necessitatibus velit nihil explicabo vero est deleniti officiis nobis neque maiores illum sit adipisci fugiat.</p>
     </div>
-    <div class="content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-    <!-- ./content -->
+</section>
 @stop
