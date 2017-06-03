7d91160b815c5314d74926f1ceaf521a8b2f36e7
Derek Nutile <dereknutile@gmail.com>
2017-06-03 07:51:38 -0700

    e/php Add storeAccount method to AccountController

--- project/app/Http/Controllers/AccountController.php
+++ project/app/Http/Controllers/AccountController.php
@@ -18,4 +18,24 @@ class AccountController extends Controller
         $rteId = "rich-text-editor";
         return view('account.index', compact('title','page_active','rteId'));
     }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function storeAccount(Request $request)
+    {
+        // $message = new Message;
+        // $message->first = $request->first;
+        // $message->last = $request->last;
+        // $message->email = $request->email;
+        // $message->subject = $request->subject;
+        // $message->message = $request->message;
+
+        // $message->save();
+        // session()->flash('flash_success','Thank you for your message.');
+        // return redirect()->route('home');
+    }
 }
