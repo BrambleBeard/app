1068268e88585f3dd479b737a380518e528c0643
Derek Nutile <dereknutile@gmail.com>
2017-06-04 08:11:12 -0700

    e/php Include vuejs

--- project/resources/views/_includes/scripts.blade.php
+++ project/resources/views/_includes/scripts.blade.php
@@ -1,4 +1,5 @@
 
 <!-- Scripts -->
-<script src="{{ asset('/assets/vendor/tinymce/tinymce.min.js' ) }}"></script>
+<script src="{{ asset('assets/vendor/tinymce/tinymce.min.js' ) }}"></script>
+<script src="{{ asset('assets/vendor/vue/dist/vue.min.js' ) }}"></script>
 <script src="{{ asset('assets/js/custom.js' ) }}"></script>
