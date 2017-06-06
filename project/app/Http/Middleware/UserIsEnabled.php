5601968fa99265663fd23bfa74d772031881b210
Derek Nutile <dereknutile@gmail.com>
2017-06-06 16:11:58 -0700

    e/php Setup UserIsEnabled middleware

--- project/app/Http/Middleware/UserIsEnabled.php
+++ project/app/Http/Middleware/UserIsEnabled.php
@@ -3,9 +3,27 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Contracts\Auth\Guard;
 
 class UserIsEnabled
 {
+    /**
+     * Implement Guard
+     *
+     * @var Guard
+     */
+     protected $guard;
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Guard $auth
+     * @return void
+     */
+     public function __construct(Guard $auth){
+         $this->guard = $auth;
+     }
+
     /**
      * Handle an incoming request.
      *
@@ -15,6 +33,11 @@ class UserIsEnabled
      */
     public function handle($request, Closure $next)
     {
+        if(! $this->guard->user()->status == "1"){
+            $this->guard->logout();
+            return redirect('/');
+        }
+
         return $next($request);
     }
 }
