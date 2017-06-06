6f924c576418ae1e4970fdd102bfa81eaec4ce79
Derek Nutile <dereknutile@gmail.com>
2017-06-06 14:06:41 -0700

    e/php Protect routes that need auth

--- project/app/Http/routes.php
+++ project/app/Http/routes.php
@@ -1,22 +1,24 @@
 <?php
 
+Route::controllers([
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController'
+]);
+
+
 /*
 |--------------------------------------------------------------------------
 | Place routes that require authentication here
 |--------------------------------------------------------------------------
 */
-Route::controllers([
-    'auth' => 'Auth\AuthController',
-    'password' => 'Auth\PasswordController'
-]);
 Route::group(['middleware' => ['auth', 'onlyenabledusers']], function () {
-    // Route::get('/account',['as'=>'account', 'uses'=>'PagesController@account']);
+    Route::get('account', ['as'=>'account', 'uses'=>'AccountController@index']);
+    Route::get('new', ['as'=>'newSecret', 'uses'=>'PagesController@newSecret']);
+    Route::post('storeAccount',['as'=>'storeAccount', 'uses'=>'PagesController@storeAccount']);
+    Route::get('saved', ['as'=>'savedSecrets', 'uses'=>'PagesController@savedSecrets']);
+    Route::post('storeSecret',['as'=>'storeSecret', 'uses'=>'PagesController@storeSecret']);
 });
 
-Route::get('account', ['as'=>'account', 'uses'=>'AccountController@index']);
-Route::post('storeAccount',['as'=>'storeAccount', 'uses'=>'PagesController@storeAccount']);
-Route::get('saved', ['as'=>'savedSecrets', 'uses'=>'PagesController@savedSecrets']);
-Route::post('storeSecret',['as'=>'storeSecret', 'uses'=>'PagesController@storeSecret']);
 
 /*
 |--------------------------------------------------------------------------
@@ -24,15 +26,16 @@ Route::post('storeSecret',['as'=>'storeSecret', 'uses'=>'PagesController@storeSe
 |--------------------------------------------------------------------------
 */
 Route::get('/', ['as'=>'home', 'uses'=>'PagesController@splash']);
-Route::get('new', ['as'=>'newSecret', 'uses'=>'PagesController@newSecret']);
 Route::get('generate', ['as'=>'generateSecret', 'uses'=>'PagesController@generateSecret']);
-
 Route::get('about',['as'=>'about', 'uses'=>'PagesController@about']);
 Route::get('contact',['as'=>'contact', 'uses'=>'PagesController@contact']);
 
-// Authentication registration routes...
+
+/*
+|--------------------------------------------------------------------------
+| Authentication registration routes...
+|--------------------------------------------------------------------------
+*/
 Route::get('login',['as'=>'login', 'uses'=>'Auth\AuthController@getLogin']);
-Route::get('logout',['as'=>'logout', 'uses'=>'Auth\AuthController@getLogout']);
+Route::get('logout',['as'=>'logout', 'uses'=>'Auth\AuthController@logout']);
 Route::get('reset',['as'=>'reset', 'uses'=>'Auth\PasswordController@getEmail']);
-// Route::get('auth/register', 'Auth\AuthController@getRegister');
-// Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
