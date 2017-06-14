0be4c736644773b04f89781879fb83514a42f2a1
Derek Nutile <dereknutile@gmail.com>
2017-06-14 06:03:53 -0700

    e/php New modal scaffold (not implemented)

---
+++ project/resources/views/_includes/navModal.php
@@ -0,0 +1,7 @@
+<div id="navModal" class="modal">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+        <h3><a href="">Test</a></h3>
+    </div>
+    <button class="modal-close"></button>
+</div>
