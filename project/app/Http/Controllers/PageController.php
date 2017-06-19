eaff2f9ac9f3b9369a21b31a92c1d3f2416767fa
Derek Nutile <dereknutile@gmail.com>
2017-06-19 05:55:23 -0700

    e/php Remove secret controller functions
    Remove all secret controller functions from the PageController.

--- project/app/Http/Controllers/PageController.php
+++ project/app/Http/Controllers/PageController.php
@@ -35,57 +35,6 @@ class PageController extends Controller
         return view('pages.contact', compact('title','page_active'));
     }
 
-    public function newSecret()
-    {
-        $title = "New Secret";
-        $page_active = "new";
-        $rteId = "rich-text-editor";
-        return view('pages.new', compact('title','page_active','rteId'));
-    }
-
-    public function savedSecrets()
-    {
-        $title = "Saved Secrets";
-        $page_active = "saved";
-        $secrets = DB::table('secrets')->get();
-        return view('pages.saved', compact('title','page_active','secrets'));
-    }
-
-    public function generateSecret()
-    {
-        $title = "Password Generator";
-        $page_active = "generate";
-        return view('pages.generate', compact('title','page_active'));
-    }
-
-    /**
-     * Store a secret.
-     *
-     * @param  Request  $request
-     * @return Response
-     */
-    public function storeSecret(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'content' => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            session()->flash('flash_info','Validation error.');
-        } else {
-            $secret = new Secret;
-            $secret->user_id = $request->user_id;
-            $secret->content = $request->content;
-            $secret->passcode = $request->passcode;
-            $secret->url = $this->createUrlHash();
-            // $secret->expires_at = $request->expires_at;
-            $secret->save();
-            session()->flash('flash_success','Secret created, url is: '.$secret->url.'.');
-        }
-
-        return redirect()->route('newSecret');
-    }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -104,32 +53,5 @@ class PageController extends Controller
         $message->save();
         session()->flash('flash_success','Thank you for your message.');
         return redirect()->route('home');
-    }
-
-    /**
-     * Create a url hash
-     *
-     * @param  Integer $length
-     * @return string
-     */
-    private function createUrlHash($length = null)
-    {
-        $source = 'abcdefghjkmnpqrstwxyzABCDEFGHKLMNOPQRSTWXYZ';
-        $url = '';
-        mt_srand((double)microtime()*1000000);
-
-        // We can force the length of the hash by passing it to the function
-        // otherwise, the length is a randomly created integer between 6 - 12
-        if(!$length)
-        {
-            $length = rand(6, 12);
-        }
-
-        while (strlen($url) < $length)
-        {
-            $url .= $source{mt_rand(0, strlen($source)-1)};
-        }
-
-        return $url;
     }
 }
