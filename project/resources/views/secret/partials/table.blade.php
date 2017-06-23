31254c9f12be1cb6aa211404e4f62bd6f17aa152
Derek Nutile <dereknutile@gmail.com>
2017-06-23 06:31:49 -0700

    e/htm Adjust columns on secret table

--- project/resources/views/secret/partials/table.blade.php
+++ project/resources/views/secret/partials/table.blade.php
@@ -2,9 +2,10 @@
 <table class="table is-striped" id="secretsTable">
     <thead>
         <tr>
-            <th>Secret</th>
-            <th>Created</th>
+            <th>Title</th>
             <th>URL</th>
+            <th>Created</th>
+            <th>Expires</th>
             <th>Actions</th>
         </tr>
     </thead>
@@ -12,8 +13,9 @@
         @foreach( $secrets as $secret)
         <tr>
             <td>{{ $secret->title }}</td>
-            <td>{{ $secret->created_at }}</td>
             <td>{{ $secret->url }}</td>
+            <td>{{ $secret->created_at }}</td>
+            <td>{{ $secret->expires_at }}</td>
             <td>[<a href="">V</a>] [<a href="">E</a>] [<a href="">D</a>]</td>
         </tr>
         @endforeach
